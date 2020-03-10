681b3c13e470fb14ab1a49525d324cb1d36bca2a
Teodor G <grigor.teodor@gmail.com>
2020-03-10 23:21:51 +0000

    Update view_class.php

--- teacher/view_class.php
+++ teacher/view_class.php
@@ -20,7 +20,6 @@
 echo '<head>';
 echo '<meta charset="utf-8">';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
-echo '<meta name="description" content="Log In to your ZeoFlow Account.">';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
 echo '<title>View Class | Quest Mode</title>';
 echo '<!-- Disable tap highlight on IE -->';
