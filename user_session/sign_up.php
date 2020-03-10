5a6bb0d4c4aad1b7d551e73df51dd7a5e601aa42
Teodor G <grigor.teodor@gmail.com>
2020-03-10 14:19:23 +0000

    Update sign_up.php

--- user_session/sign_up.php
+++ user_session/sign_up.php
@@ -15,7 +15,7 @@
         $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
         $registerUser=$db->registerUser($emailData, $passwordData, $usernameData, $universityData,
             getIPDetails()->geoplugin_countryCode);
-        echo $registerUser["institution_id"][0];
+        echo $registerUser["error"];
     }
     exit;
   }
