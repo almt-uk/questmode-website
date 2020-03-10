1b858cb01481a72d55ac70231f4df2281141e5e6
Teodor G <grigor.teodor@gmail.com>
2020-03-10 23:20:28 +0000

    Update view_class.php

--- teacher/view_class.php
+++ teacher/view_class.php
@@ -1,4 +1,22 @@
 <?php
+  session_start();
+  if(!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn'])
+  {
+    header("Location: ../",  true);
+    exit;
+  }
+  else if(isset($_SESSION["userData"]))
+  {
+    $userData = json_decode($_SESSION["userData"]);
+    if(!$userData->is_teacher)
+    {
+        header("Location: ../",  true);
+        exit;
+    }
+  }
+?>
+
+<?php
 echo '<head>';
 echo '<meta charset="utf-8">';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
