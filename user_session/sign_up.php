b11dac87b4628ff57c3986dd3ebe8424b5cda4ad
Teodor G <grigor.teodor@gmail.com>
2020-03-10 13:06:14 +0000

    Update sign_up.php

--- user_session/sign_up.php
+++ user_session/sign_up.php
@@ -13,6 +13,7 @@
         $db = new DbHandlerWeb();
         $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
         $registerUser=$db->registerUser($emailData, $passwordData, $usernameData, $universityData);
+        echo $registerUser;
     }
     exit;
   }
