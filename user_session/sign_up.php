1046f4038a42845657015ba3ba92f17d8aa7e4c6
Teodor G <grigor.teodor@gmail.com>
2020-03-10 12:28:06 +0000

    Update sign_up.php

--- user_session/sign_up.php
+++ user_session/sign_up.php
@@ -12,7 +12,7 @@
         require_once $path . '/db_handler/web.php';
         $db = new DbHandlerWeb();
         $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
-        echo $db->registerUser($emailData, $passwordData, $usernameData, $universityData)["error"];
+        $registerUser=$db->registerUser($emailData, $passwordData, $usernameData, $universityData);
     }
     exit;
   }
