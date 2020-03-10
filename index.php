d44280be4112fce02da0e12fca6a4efb4b65643a
Teodor G <grigor.teodor@gmail.com>
2020-03-10 15:53:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ echo 'Sign Up';
 echo '</div>';
 echo '<script type="text/javascript">';
 echo 'function goRegister() {';
-echo 'window.location = "sign_up";';
+echo 'window.location = "signup";';
 echo '}';
 echo '</script>';
 echo '<div class="toolbarUserSessionLogIn" onclick="goLogin();">';
