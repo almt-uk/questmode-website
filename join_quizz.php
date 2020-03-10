8a6337abf4c8534858ae860d925be95b1c22b303
Teodor G <grigor.teodor@gmail.com>
2020-03-10 22:56:19 +0000

    Update join_quizz.php

--- join_quizz.php
+++ join_quizz.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if(isset($_SESSION["userData"]) && isset($_SESSION["isLoggedIn"]))
+  if(!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn'])
   {
     header("Location: /",  true);
     exit;
