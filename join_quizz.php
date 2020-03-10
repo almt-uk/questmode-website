4624eb2af7433cfae3dbf8a3082ef73e330cc140
Teodor G <grigor.teodor@gmail.com>
2020-03-10 22:48:10 +0000

    Update join_quizz.php

--- join_quizz.php
+++ join_quizz.php
@@ -4,7 +4,6 @@ echo '<meta charset="utf-8">';
 echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
 echo '<title>Join Quest</title>';
-echo '';
 echo '<!-- Disable tap highlight on IE -->';
 echo '<meta name="msapplication-tap-highlight" content="no">';
 echo '';
