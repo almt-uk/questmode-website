672e865286268cd8db64192a00496ac7cea8fc18
Teodor G <grigor.teodor@gmail.com>
2020-03-11 00:10:24 +0000

    Update create_quizz.php

--- teacher/create_quizz.php
+++ teacher/create_quizz.php
@@ -21,7 +21,6 @@
         $quizzData=$_POST['quizzData'];
         $questions=$_POST['questions'];
         require_once $path . '/db_handler/web.php';
-        require_once $path . '/libs/Utils/ip_details.php';
         $db = new DbHandlerWeb();
         $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
         $db->publishQuizz($quizzData, $questions, $userData->user_id);
