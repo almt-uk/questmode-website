96aaf7ae07ca9370ecee6f44215dd248e7c75e32
Teodor G <grigor.teodor@gmail.com>
2020-03-11 01:27:24 +0000

    Update answer_results.php

--- quizz/answer_results.php
+++ quizz/answer_results.php
@@ -57,6 +57,12 @@
   $is_right4 = $questionOneDetails["is_right"];
   $order_id4 = $questionOneDetails["order_id"];
   
+  $path = $_SERVER['DOCUMENT_ROOT'];
+  require_once $path . '/db_handler/web.php';
+  $db = new DbHandlerWeb();
+  $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
+  
+  
 ?>
 
 <?php
