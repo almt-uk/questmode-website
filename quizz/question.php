9118c8dd3f0d9719b34bdf2248ae0d90a1a5e47e
Teodor G <grigor.teodor@gmail.com>
2020-03-11 01:07:41 +0000

    Update question.php

--- quizz/question.php
+++ quizz/question.php
@@ -29,12 +29,33 @@
   $time_results = $questionData["time_results"];
   $questionAnswersData = $questionData["questionAnswersData"];
   
-  $questionOneDetails = $questionAnswersData[0];
-  $answer_id = $questionOneDetails["answer_id"];
-  $question_id = $questionOneDetails["question_id"];
-  $content = $questionOneDetails["content"];
-  $is_right = $questionOneDetails["is_right"];
-  $order_id = $questionOneDetails["order_id"];
+  $answerDetails1 = $questionAnswersData[0];
+  $answer_id1 = $questionOneDetails["answer_id"];
+  $question_id1 = $questionOneDetails["question_id"];
+  $content1 = $questionOneDetails["content"];
+  $is_right1 = $questionOneDetails["is_right"];
+  $order_id1 = $questionOneDetails["order_id"];
+  
+  $answerDetails2 = $questionAnswersData[1];
+  $answer_id2 = $questionOneDetails["answer_id"];
+  $question_id2 = $questionOneDetails["question_id"];
+  $content2 = $questionOneDetails["content"];
+  $is_right2 = $questionOneDetails["is_right"];
+  $order_id2 = $questionOneDetails["order_id"];
+  
+  $answerDetails3 = $questionAnswersData[2];
+  $answer_id3 = $questionOneDetails["answer_id"];
+  $question_id3 = $questionOneDetails["question_id"];
+  $content3 = $questionOneDetails["content"];
+  $is_right3 = $questionOneDetails["is_right"];
+  $order_id3 = $questionOneDetails["order_id"];
+  
+  $answerDetails4 = $questionAnswersData[3];
+  $answer_id4 = $questionOneDetails["answer_id"];
+  $question_id4 = $questionOneDetails["question_id"];
+  $content4 = $questionOneDetails["content"];
+  $is_right4 = $questionOneDetails["is_right"];
+  $order_id4 = $questionOneDetails["order_id"];
   
 ?>
 
