4a1f0277d773fe7290779024eb6c5ebdc416085b
Teodor G <grigor.teodor@gmail.com>
2020-03-11 03:38:36 +0000

    Update sign_up.php

--- user_session/sign_up.php
+++ user_session/sign_up.php
@@ -245,6 +245,9 @@ echo 'cursor: pointer;';
 echo '}';
 echo '</style>';
 echo '</head>';
+?>
+
+<?php
 echo '<body>';
 echo '<div class="toolbarHolder toolbarShadow unselectable" id="toolbar">';
 echo '<div class="toolbarContent">';
