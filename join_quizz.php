544929d619e075c9554c4f4c34d8b1715f76e23e
Teodor G <grigor.teodor@gmail.com>
2020-03-11 00:25:02 +0000

    Update join_quizz.php

--- join_quizz.php
+++ join_quizz.php
@@ -32,7 +32,11 @@
         require_once $path . '/db_handler/web.php';
         $db = new DbHandlerWeb();
         $db->initializeAPI("xtoAkWqVGp4nDtW6tZL1AaJUCl9I3tYcqjfTBhSu", "PHZ7dh4vHtbJoF7kD2RtZQUxi3opTFeXvpa0Jp7R");
-        $db->publishQuizz($quizzData, $questions, $userData->user_id);
+        $joinQuizz = $db->joinQuizz($quizzCode, $quizzNickname);
+        if(!$joinQuizz["error"])
+        {
+
+        }
         exit;
     } else {
         exit;
