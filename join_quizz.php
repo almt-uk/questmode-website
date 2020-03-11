0bfd68fea215effc003bebc824aad9b342ac891e
Teodor G <grigor.teodor@gmail.com>
2020-03-11 04:07:56 +0000

    Update join_quizz.php

--- join_quizz.php
+++ join_quizz.php
@@ -215,7 +215,7 @@ echo 'Join Quizz';
 echo '</div>';
 echo '<script type="text/javascript">';
 echo 'function goJoinQuizz() {';
-echo 'window.location = "join";';
+echo 'window.location = "/quizz/join";';
 echo '}';
 echo '</script>';
 echo '<div class="userLoggedInOptionsItem">';
